1583e0708d7bc0649ce79730dd26c36b5283b7aa
harumad <rumadstar@gmail.com>
2025-10-30 12:06:47 +0000

    add tailwind to my react app

---
+++ WEEK1/assignments/reactjs-practice/src/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores beatae repellat doloribus quisquam tenetur perferendis nam commodi eum distinctio quam similique vitae qui neque nesciunt ab, officiis iste dignissimos eaque?</h1>;
+    </div>
+  )
+}
+
+export default 
+<h1>hello this is Joshua</h1>;
---
+++ WEEK2/exercises/react-ex2/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-blue-400">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+        fugiat fugit porro laborum laudantium nisi dolor sapiente iusto
+        laboriosam vitae facere placeat perferendis, autem optio temporibus
+        veniam reprehenderit maiores molestias.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
